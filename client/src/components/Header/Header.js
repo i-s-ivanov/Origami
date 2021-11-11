e5bd16637916485e30bb970913d391ef3fa25707
Iskren Ivanov <pecito_vn@hotmail.com>
2021-11-11 17:39:50 +0200

    Add header navigation

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,26 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import NavigationItem from './NavigationItem/NavigationItem';
+import style from './Header.module.css';
+
+const Header = () => {
+    return (
+        <nav className={style.navigation}>
+            <ul>
+                <li className="listItem"><img src="/white-origami-bird.png" alt="white origami" /></li>
+                <NavigationItem>Going to 1</NavigationItem>
+                <NavigationItem>Going to 2</NavigationItem>
+                <NavigationItem>Going to 3</NavigationItem>
+                <NavigationItem>Going to 4</NavigationItem>
+                <NavigationItem>Going to 5</NavigationItem>
+                <NavigationItem>Going to 6</NavigationItem>
+                <NavigationItem>Going to 7</NavigationItem>
+                <NavigationItem>Going to 8</NavigationItem>
+                <NavigationItem>Going to 9</NavigationItem>
+                <NavigationItem>Going to 10</NavigationItem>
+                <NavigationItem>Going to 11</NavigationItem>
+            </ul>
+        </nav>
+    );
+};
+
+export default Header;
---
+++ client/src/components/Header/NavigationItem/NavigationItem.js
@@ -0,0 +1,13 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import style from './NavigationItem.module.css';
+
+const NavigationItem = (props) => {
+    return (
+        <li className={style.listItem}>
+            <a href="#" className={style.navListItem}>{props.children}</a>
+        </li>
+    );
+};
+
+
+export default NavigationItem;
